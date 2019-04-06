0a5adc552cc54e41e7bcf09e4b4eba89176e6ca5
eronmmer <eronmmer@gmail.com>
2019-04-06 19:32:52 +0100

    remove img closing tag

--- hng/Welcome.php
+++ hng/Welcome.php
@@ -9,9 +9,9 @@
 		<br>
 		<h4 id="welcome-info">Create an account to share and read great stories, personalize your journal, and follow authors that you love.</h4>
 		<br>
-		<button id="google-signup"><img src='../../images/googleic.jpg' id="google-icon"></img> Sign up with Google </button>
+		<button id="google-signup"><img src='../../images/googleic.jpg' id="google-icon"> Sign up with Google </button>
 		<br>
-		<button id="fb-signup"><img src='../../images/fbic.png' id="fb-icon"></img> Sign up with Facebook </button>
+		<button id="fb-signup"><img src='../../images/fbic.png' id="fb-icon"> Sign up with Facebook </button>
 		<h6 >To make this site work, we log user data and share it with service providers.</h6>
 		</div>
 	</div>
