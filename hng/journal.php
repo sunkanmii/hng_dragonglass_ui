f8204eea1c4552c4e5cb926e82a2013e34940f5a
Daniel Don <dandons25@gmail.com>
2019-04-06 18:24:33 +0100

    Added the Journal Page

---
+++ hng/journal.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+	<html>
+        <?php include'includes/head.php'; ?>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<body>
+		<div class="parent">
+			<div class="container padd">
+				<div class="row">
+					<div class="col-sm-3">
+						<?php include'includes/nav.php'; ?>
+					</div>
+
+					<div class="col-sm-9">
+						<div class="row">
+							<div class="col-sm-6">
+								<h4 class="text-left my-journal">MY JOURNAL</h4>
+							</div>
+							<div class="col-sm-6">
+								<h4 class="text-right"><a href="/" class="was-cta">Write a story</a></h4>
+							</div>
+						</div>
+						<br><br>
+						<div>
+							<div class="draft-heading">
+								<!-- replace 10 in sub tag with a php variable for number of published works -->
+								<span class="published-link"><a href="/">Published</a><sub class="no_of_pub font-italic">10</sub></span> <a href="/" class="drafts-link">Drafts</a> <hr>
+							</div>
+
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <p class="journal-post-date">April 10th, 2019</p>
+                                            <h3>Is Software Eating The World? <i class=" elipse-margin fa fa-ellipsis-v"></i></h3>
+                                        </div>
+                                    </div>
+                                    <div class="row post">
+                                        <div class="col-sm-10">
+                                            <p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+                                        </div>
+                                    </div> <hr>
+                                </div>
+                                
+
+                                <div class="col-sm-12">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <p class="journal-post-date">April 10th, 2019</p>
+                                            <h3>Time-saving css techniques for responsive images <i class=" elipse-margin fa fa-ellipsis-v"></i></h3>
+                                        </div>
+                                    </div>
+                                    <div class="row post">
+                                        <div class="col-sm-10">
+                                            <p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+                                        </div>
+                                    </div> <hr>
+                                </div>
+
+                                <div class="col-sm-12">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <p class="journal-post-date">April 10th, 2019</p>
+                                            <h3>For People Still Hesitating To learn Design <i class=" elipse-margin fa fa-ellipsis-v"></i></h3>
+                                        </div>
+                                    </div>
+                                    <div class="row post">
+                                        <div class="col-sm-10">
+                                            <p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+                                        </div>
+                                    </div> <hr>
+                                </div>
+
+                                <div class="col-sm-12">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <p class="journal-post-date">April 10th, 2019</p>
+                                            <h3>Why Figma Is Better Than Sketch <i class=" elipse-margin fa fa-ellipsis-v"></i></h3>
+                                        </div>
+                                    </div>
+                                    <div class="row post">
+                                        <div class="col-sm-10">
+                                            <p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+                                        </div>
+                                    </div> <hr>
+                                </div>
+
+                                <div class="col-sm-12">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <p class="journal-post-date">April 10th, 2019</p>
+                                            <h3>Is Software Eating The World? <i class=" elipse-margin fa fa-ellipsis-v"></i></h3>
+                                        </div>
+                                    </div>
+                                    <div class="row post">
+                                        <div class="col-sm-10">
+                                            <p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+
+
+
+
+
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	</html>	
+	
