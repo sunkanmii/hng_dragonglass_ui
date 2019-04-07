d8514bdc7965e6bcd63cdeebd520296ab6edadda
Yusuff, Olawumi Qauzeem <36412651+Qausim@users.noreply.github.com>
2019-04-07 11:46:54 +0100

    Please review my changes (#34)
    * Handled clearing the editor's content if it is the default content set
    
    * The editor is now focused on automatically on tool click and default content set back when user nagivates away.
    
    * Updated the code to take care of editor's 'p' tag removed.

--- hng/app.js
+++ hng/app.js
@@ -1,3 +1,4 @@
+
 const toolbar = document.querySelector('.toolbar')
 const editor = document.querySelector('.editor')
 editor.addEventListener('click', (e)=> {
@@ -5,3 +6,24 @@ editor.addEventListener('click', (e)=> {
         toolbar.style.opacity = 1
     }, 500)
 })
+
+const editorDefaultText = editor.textContent.trim();
+// Focus listener on editor
+// If its content is the default content, it clears the content.
+editor.addEventListener('focus', () => {
+    if (editor.textContent.trim() === editorDefaultText) {
+        editor.textContent = "";
+        editor.click();
+    }
+});
+
+// Set editor default content if text was not entered and user navigated away
+editor.addEventListener('blur', () => {
+    let content = editor.textContent;
+
+    editor.textContent = content ? content : editorDefaultText;
+});
+
+// Focus on editor on tool click
+const toolbarTools = Array.prototype.slice.call(document.querySelectorAll('.toolbar .tool'));
+toolbarTools.forEach(el => el.addEventListener('click', () => editor.focus()));
